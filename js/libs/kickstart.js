279a234552c1c13d013895d1c9b46148d728c1ff
Pablo Damian Cotoia <datamosh@me.com>
2012-12-10 17:34:10 -0200

    Update js/libs/kickstart.js
    New version

--- js/libs/kickstart.js
+++ js/libs/kickstart.js
@@ -113,9 +113,9 @@ if (!('some' in Array.prototype)) {
 	}
 }
 
-// Javacript kickstart object 0.1
+// Javacript kickstart object 0.2
 var kickstart = {
-	version: '0.1',
+	version: '0.2',
 
 	// Utils
 	util: {
@@ -161,6 +161,33 @@ var kickstart = {
 		// Get random integer
 		getRandomInt: function(min, max) {
 			return Math.floor(Math.random() * (max - min + 1)) + min;
+		},
+
+		// Format number
+		separateNumber: function(number, separator) {
+			if(!separator) separator = ','
+			var str = number.toString().split('.')
+			if (str[0].length >= 4)
+			str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1' + separator)
+			if (str[1] && str[1].length >= 4)
+				str[1] = str[1].replace(/(\d{3})/g, '$1 ')
+			return str.join('.')
+		},
+
+		// Pad number
+		pad: function(number, width, character) {
+			character = character || '0';
+			number = number + '';
+			return number.length >= width ? number : new Array(width - number.length + 1).join(character) + number;
+		}
+	},
+
+	// Validation
+	validate: {
+		// Validate email
+		email: function(email) {
+			var regexp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i
+			return regexp.test(email)
 		}
 	},
 
